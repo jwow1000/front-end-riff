eed56f3f71db1cfab1e47790b27d5be731d4ad7e
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-04-08 14:50:32 -0400

    App.jsx is prepared

--- src/components/PostPreview/PostPreview.jsx
+++ src/components/PostPreview/PostPreview.jsx
@@ -2,12 +2,22 @@
 
 import './PostPreview.css';
 
-function PostPreview() {
+function PostPreview( {post} ) {
+  const imgStyle = {
+    'background-img': `url(${post.image})`
+  }
+  
   return (
-    <div>
-      
+    <div 
+      id="mainContainer-PostPreview"
+      style={imgStyle}
+    >
+      <div id="profilePicContainer-PostPreview">
+        <img src={post.user.profile_pic} alt="profile pic of you, the user" />  
+      </div>
+
     </div>
   )
 }
 
-export default PostPreview
+export default PostPreview  
