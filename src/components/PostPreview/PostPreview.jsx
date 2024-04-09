4cce2d9c950d3ca34e1b8e86fa2b6cee0d9f6fbb
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-04-09 12:15:18 -0400

    more css work

--- src/components/PostPreview/PostPreview.jsx
+++ src/components/PostPreview/PostPreview.jsx
@@ -4,25 +4,39 @@ import {useState, useEffect} from 'react';
 import './PostPreview.css';
 
 function PostPreview( {post} ) {
-  const [profLink, setProfLink] = useState("");
+  const [profileImg, setProfileImg] = useState("");
+  const [postImg, setPostImg] = useState("");
   
   useEffect( () => {
-    setProfLink("https://i.ytimg.com/vi/PuTBDeatxSM/maxresdefault.jpg")
-  }, [])
+    setProfileImg("https://www.hdwallpaper.nu/wp-content/uploads/2015/11/Spongebob_Squarepants_wallpaper_017.jpg");
+    setPostImg("https://i.ytimg.com/vi/PuTBDeatxSM/maxresdefault.jpg");
+
+  }, []);
   
+  const handleClick = () => {
+    console.log('i;ve been clicked');
+  }
   
   return (
-    
-    
     <div 
       id="mainContainer-PostPreview"
       style={{
-       
-        'backgroundImage': `linear-gradient(rgba(45,255,196,0.2), rgba(15, 15, 100, 0.5)), url(${profLink})`
+        'backgroundImage': `linear-gradient(rgba(45,255,196,0.2), rgba(15, 15, 100, 0.5)), url(${postImg})`
       }}
+      onClick={handleClick}
     >
+      <div className='sidebarBg-PostPreview'>
+        <div 
+            id="profPic-PostPreview"
+            style={{'backgroundImage': `url(${profileImg})`}}
+            alt="your profile pic"
+          > 
+        </div>       
+      </div>
       
+      <div className='sidebarBg-PostPreview'>
 
+      </div>
     </div>
   )
 }
